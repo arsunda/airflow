416a4bbbdd79831f5bc987f00718cbc9325ef1f9
Ambika Garg <v-ambikagarg@microsoft.com>
2024-10-16 14:44:22 -0400

    files added by precommit checks

--- providers/tests/system/microsoft/azure/example_mock_test.py
+++ providers/tests/system/microsoft/azure/example_mock_test.py
@@ -40,7 +40,7 @@ with DAG(
     task_a = EmptyOperator(task_id="test_task_a")
 
 
-from tests.system.utils import get_test_run  # noqa: E402
+from tests_common.test_utils.system_tests import get_test_run  # noqa: E402
 
 # Needed to run the example DAG with pytest (see: tests/system/README.md#run_via_pytest)
 test_run = get_test_run(dag)
