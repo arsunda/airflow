0cd03ebd26a5acd9afd43ee2b3b438082a2f26d1
Ambika Garg <v-ambikagarg@microsoft.com>
2024-12-10 14:05:55 -0500

    Refactor system dashboard

--- .github/scripts/process_artifact.py
+++ .github/scripts/process_artifact.py
@@ -9,8 +9,53 @@ from azure.storage.blob import BlobServiceClient
 XML_ARTIFACT_PATH = "pytest-report/pytest-report.xml"
 JSON_OUTPUT_PATH = "pytest-report/processed-report.json"
 
+def upload_blob(connection_string: str, container_name: str, blob_name: str, file_path: str) -> None:
+    """
+    Upload a file to Azure Blob Storage.
+
+    :param connection_string: Provide the connection string to the Azure Storage account.
+    :param container_name: Specify the name of the container where the blob will be uploaded.
+    :param blob_name: Define the name of the blob in the container.
+    :param file_path: Indicate the path to the local file to be uploaded.
+    :raises ResourceExistsError: Raise if the blob already exists and the upload is not allowed to overwrite.
+    """
+    try:
+        # Create a BlobServiceClient
+        blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+
+        # Create a ContainerClient
+        container_client = blob_service_client.get_container_client(container_name)
+
+        # Create a BlobClient
+        blob_client = container_client.get_blob_client(blob_name)
+
+        # Upload the file
+        with open(file_path, "rb") as data:
+            blob_client.upload_blob(
+                data, overwrite=True
+            )  # Set overwrite=True to replace if blob already exists
+
+        print(f"File '{file_path}' uploaded to blob '{blob_name}' in container '{container_name}'.")
+
+    except Exception as e:
+        raise ValueError(f"An error occurred while uploading the blob to Gold container {container_name}: {e}")
+
 
 def parse_xml_to_json(xml_content):
+    """
+    Parses XML content and converts it to a JSON-like structure.
+    Args:
+        xml_content (str): A string containing XML data.
+    Returns:
+        tuple: A tuple containing:
+            - testcases (list): A list of dictionaries, each representing a testcase with the following keys:
+                - name (str): The name of the testcase.
+                - time (str): The time taken for the testcase.
+                - classname (str): The classname of the testcase.
+                - failure (str): The failure message of the testcase, if any.
+                - timestamp (str): The timestamp of the testsuite.
+            - timestamp (str): The timestamp of the testsuite.
+    """
     # Parse XML content
     root = ET.fromstring(xml_content)
 
@@ -46,49 +91,46 @@ def parse_xml_to_json(xml_content):
             for testcase in testsuite["testcase"]:
                 testcaseobj = {}
                 testcaseobj["name"] = testcase.get("name", None)
-                testcaseobj["time"] = testcase.get("time", None)
+                testcaseobj["time"] = testcase.get("time", None) # Time taken for the testcase to run
                 testcaseobj["classname"] = testcase.get("classname", None)
                 testcaseobj["failure"] = testcase.get("failure", None)
-                testcaseobj["timestamp"] = timestamp
+                testcaseobj["timestamp"] = timestamp # Timestamp of the testsuite
 
                 testcases.append(testcaseobj)
+                
+                # TODO: If test is a failure, we can send a notification to the team
 
     return testcases, timestamp
 
 
-def upload_blob(connection_string: str, container_name: str, blob_name: str, file_path: str) -> None:
-    """
-    Upload a file to Azure Blob Storage.
-
-    :param connection_string: Provide the connection string to the Azure Storage account.
-    :param container_name: Specify the name of the container where the blob will be uploaded.
-    :param blob_name: Define the name of the blob in the container.
-    :param file_path: Indicate the path to the local file to be uploaded.
-    :raises ResourceExistsError: Raise if the blob already exists and the upload is not allowed to overwrite.
-    """
-    try:
-        # Create a BlobServiceClient
-        blob_service_client = BlobServiceClient.from_connection_string(connection_string)
-
-        # Create a ContainerClient
-        container_client = blob_service_client.get_container_client(container_name)
-
-        # Create a BlobClient
-        blob_client = container_client.get_blob_client(blob_name)
-
-        # Upload the file
-        with open(file_path, "rb") as data:
-            blob_client.upload_blob(
-                data, overwrite=True
-            )  # Set overwrite=True to replace if blob already exists
-
-        print(f"File '{file_path}' uploaded to blob '{blob_name}' in container '{container_name}'.")
-
-    except Exception as e:
-        print(f"An error occurred: {e}")
-
-
 def process_file(file_path):
+    """
+    Processes an XML file, converts its content to JSON, and generates a blob name based on current timestamp.
+
+    Args:
+        file_path (str): The path to the XML file to be processed.
+
+    Returns:
+        tuple: A tuple containing:
+            - blob_name (str): The name of the blob to be uploaded, based on the timestamp when test was executed.
+            - json_data (str): JSON list in the below format:
+            [
+                {
+                    "name": "test_run",
+                    "time": "36.376",
+                    "classname": "providers.tests.system.microsoft.fabric.example_fabric_notebook_run",
+                    "failure": null,
+                    "timestamp": "2024-12-09T22:15:02.123111+00:00"
+                },
+                {
+                    "name": "test_run",
+                    "time": "2.525",
+                    "classname": "providers.tests.system.microsoft.fabric.example_fabric_pipeline_run",
+                    "failure": null,
+                    "timestamp": "2024-12-09T22:15:02.123111+00:00"
+                }
+            ]
+    """
     if os.path.isfile(file_path):
         with open(file_path) as file:
             xml_content = file.read()
@@ -111,19 +153,21 @@ def save_json(json_data, file_path):
 
 if __name__ == "__main__":
     connection_string = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
-    output_container_name = "airflow-system-dashboard-output"
+    gold_container_name = os.getenv("GOLD_AZURE_STORAGE_CONTAINER_NAME")
 
     if not connection_string:
         raise ValueError("No connection string provided for Azure Blob Storage.")
+    if gold_container_name is None:
+        raise ValueError("Output container name is not provided for Azure Blob Storage.")
 
     if os.path.exists(XML_ARTIFACT_PATH):
         blob_name, json_data = process_file(XML_ARTIFACT_PATH)
-
+ 
         save_json(json_data, JSON_OUTPUT_PATH)
 
         upload_blob(
             connection_string=connection_string,
-            container_name=output_container_name,
+            container_name=gold_container_name,
             blob_name=blob_name,
             file_path=JSON_OUTPUT_PATH,
         )
--- .github/scripts/process_last_k_runs.py
+++ .github/scripts/process_last_k_runs.py
@@ -1,41 +1,85 @@
 from __future__ import annotations
 
-import json
 import os
+import json
+from datetime import datetime, timedelta, timezone
 
 from azure.storage.blob import BlobServiceClient
 
 
-def preprocess_blob_data(report_group_by_testcases: dict, test_run_data) -> None:
+def preprocess_blob_data(report_group_by_testcases: dict[str, dict], test_run_data: str, k: int) -> None:
     """
-    Preprocess the blob data to extract the required information.
+    Preprocess the blob data to extract the timestamp, status of test run and last run duration.
 
-    :param report_group_by_testcases: The consolidated dict to store the test run data.
-    :param test_run_data: Test run data to added in the consolidated dict `report group by testcases`.
-    :return: None.
+    :param report_group_by_testcases: 
+        A consolidated dictionart to store the test run data.
+        Expected structure:
+        {
+            "TestCaseName": {
+                "last_runs": [
+                    {
+                        "timestamp": str,  # ISO format datetime string
+                        "type": str  # "failure", "success", etc.
+                    },
+                    ...
+                ],
+                "last_run_duration": float,  # Duration in seconds
+                "InvocationCount": int,  # Number of times the test case was invoked in a week window
+                "FailureCount": int,  # Number of times the test case failed in a week window
+            }
+        }
+        
+    :param test_run_data: 
+        A JSON string containing test run details. 
+        Expected structure:
+        [
+            {
+                "classname": str,       # Name of the test case
+                "timestamp": str,       # ISO format datetime string
+                "failure": bool,        # True if the test failed, else False
+                "time": float           # Duration of the test in seconds
+            },
+            ...
+        ]
+        
+    :param k: 
+        The number of last runs to be considered for each test case.
+    
+    :return: Updates the report_group_by_testcases dict with the test run data inplace.
     """
     test_run = json.loads(test_run_data)
     for _index, item in enumerate(test_run):
         testname = item["classname"]
         if testname not in report_group_by_testcases:
-            # initialize new test case.
+            # Initialize new test case.
             report_group_by_testcases[testname] = {
                 "last_runs": [
                     {
                         "timestamp": item["timestamp"],
                         "type": "failure"
                         if item["failure"]
-                        else "success",  # Improve it, need to consider all the test cases like failure, success, skipped, cancelled, etc.
+                        else "success",  # TODO: Need to consider all the test cases like failure, success, skipped, cancelled, etc.
                     }
                 ],
                 "last_run_duration": item["time"],
+                "InvocationCount": 1,
+                "FailureCount": 1 if item["failure"] else 0,
+                "SuccessCount": 0 if item["failure"] else 1,
             }
         else:
-            report_group_by_testcases[testname]["last_runs"].append(
-                {"timestamp": item["timestamp"], "type": "failure" if item["failure"] else "success"}
-            )
-            # # Overwrite the last run duration as blobs are sorted by timestamp
-            # report_group_by_testcases[testname]["last_run_duration"] = item["time"]
+            # Update existing test case.
+            if len(report_group_by_testcases[testname]["last_runs"]) < k:
+                report_group_by_testcases[testname]["last_runs"].append(
+                    {"timestamp": item["timestamp"], "type": "failure" if item["failure"] else "success"}
+                )
+            
+            report_group_by_testcases[testname]["InvocationCount"] += 1
+            if item["failure"]:
+                report_group_by_testcases[testname]["FailureCount"] += 1
+            else:
+                report_group_by_testcases[testname]["SuccessCount"] += 1
+            
+    return
 
 
 def upload_blob(connection_string: str, container_name: str, blob_name: str, data: str) -> None:
@@ -64,62 +108,124 @@ def upload_blob(connection_string: str, container_name: str, blob_name: str, dat
         print(f"Data uploaded to blob '{blob_name}' in container '{container_name}'.")
 
     except Exception as e:
-        print(f"An error occurred: {e}")
+        raise ValueError(f"An error occurred while uploading the blob to Gold container {container_name}: {e}")
 
+def fetch_all_blobs(connection_string: str, container_name: str) -> list:
+    """
+        Fetch all the blobs from the Azure blob storage container.
+        Returns:
+            List of blob names in the container and timestamp.
+    """
+    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+    container_client = blob_service_client.get_container_client(container_name)
+    
+    try:
+        blobs = []
+        blob_list = container_client.list_blobs()
+        
+        for blob in blob_list:
+            # Extract timestamp from the blob name
+            if blob.name.startswith("pytest-report-") and blob.name.endswith(".json"):
+                try:
+                    timestamp_str = blob.name.split("pytest-report-")[1].replace(".json", "")
+                    blob_timestamp = datetime.fromisoformat(timestamp_str)
+                    blobs.append({"name": blob.name, "timestamp": blob_timestamp})
+                except ValueError:
+                    print(f"Skipping invalid blob: {blob.name}")
+                    
+        return blobs
+    except Exception as e:
+        raise ValueError(f"An unexpected error occured while fetching the blobs from Azure Blob storage container {container_name}: {e}")
+
+def download_blob(connection_string: str, container_name: str, blob_name: str) -> str:
+    """
+    Download a blob from Azure Blob Storage.
+
+    :param connection_string: Provide the connection string to the Azure Storage account.
+    :param container_name: Specify the name of the container where the blob is stored.
+    :param blob_name: Specify the name of the blob to be downloaded.
+    :return: The content of the blob.
+    """
+    try:
+        # Create a BlobServiceClient
+        blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+
+        # Create a ContainerClient
+        container_client = blob_service_client.get_container_client(container_name)
+
+        # Create a BlobClient
+        blob_client = container_client.get_blob_client(blob_name)
+
+        # Download the blob
+        blob_data = blob_client.download_blob().readall()
+
+        return blob_data
+
+    except Exception as e:
+        raise ValueError(f"An error occurred while downloading the blob {blob_name} from container {container_name}: {e}")
 
 def consolidate_runs(connection_string: str, container_name: str, k=10) -> None:
     """
-    List all blobs in the specified Azure Blob Storage container.
+    Combine the last k runs of the test cases and consolidate them into a single report.
 
     :param connection_string: Connection string to the Azure Blob Storage account.
     :param container_name: The name of the container in which blobs are listed.
-    :return: None
+
+    :return: The consolidated report of the last k runs of the test cases.
     """
-    # Create a BlobServiceClient object using the connection string
-    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+    # A test run contains the run report of the system tests present in the Microsoft Fabric Apache Airflow repository.
+    # Each test run is represented as a blob in the storage container. The blob includes detailed status and results 
+    # for all system tests executed during that particular run.
+    
+    # Fetch all blobs from azure blob storage container
+    blobs = fetch_all_blobs(connection_string, container_name)
+    
+    # Sort the blobs by timestamp in descending order
+    blobs.sort(key=lambda b: b["timestamp"], reverse=True)
+    
+    # Calculate one week ago
+    one_week_ago = datetime.now(timezone.utc) - timedelta(weeks=1)
 
-    # Get the container client
-    container_client = blob_service_client.get_container_client(container_name)
-
+    # Filter blobs that are one week old
+    one_week_old_blobs = [blob for blob in blobs if blob["timestamp"] >= one_week_ago]
+    
+    # Prepare the consolidated report
     report_group_by_testcases: dict = {}
+    
     try:
-        blob_count = 0
-        blob_list = container_client.list_blobs()
-        if blob_list is not None:
-            sorted_blobs = sorted(blob_list, key=lambda b: b.name, reverse=True)
-            for blob in sorted_blobs:
-                if blob_count > k:
-                    break
-                blob_client = container_client.get_blob_client(blob)  # type: ignore
-                test_run_data = blob_client.download_blob().readall()
-                blob_count += 1
+        for blob in one_week_old_blobs:
+            test_run_data = download_blob(connection_string, container_name, blob["name"])
 
-                # Preprocess the blob data - this will add the test run to the list of runs for a test case
-                preprocess_blob_data(report_group_by_testcases, test_run_data)
+            # Preprocess the blob data - this will add the test run to the list of runs for a test case
+            preprocess_blob_data(report_group_by_testcases, test_run_data, k)
 
-            # Convert the dictionary to a list
-            report_group_by_testcases_list = [
-                {"name": key, **value} for key, value in report_group_by_testcases.items()
-            ]
-
-            upload_blob(
-                connection_string,
-                "consolidate-blob",
-                "gold-report.json",
-                json.dumps(report_group_by_testcases_list),
-            )
-        else:
-            print("No blobs found or container doesn't exist.")
+        # Convert the dictionary to a list
+        report_group_by_testcases_list = [
+            {"name": key, **value} for key, value in report_group_by_testcases.items()
+        ]
+        
+        return report_group_by_testcases_list
     except Exception as e:
-        print(f"An error occurred: {e}")
+        raise ValueError(f"An error occurred: {e}")
 
 
-# Example usage
 if __name__ == "__main__":
     connection_string = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
-    container_name = "airflow-system-dashboard-output"
+    silver_container_name = os.getenv("SILVER_AZURE_STORAGE_CONTAINER_NAME")
+    gold_container_name = os.getenv("GOLD_AZURE_STORAGE_CONTAINER_NAME")
 
     if connection_string is None:
         raise ValueError("Connection string is not provided for Azure blob storage.")
+    if silver_container_name is None:
+        raise ValueError("Silver container name is not provided for Azure Blob Storage.")
+    if gold_container_name is None:
+        raise ValueError("Gold container name is not provided for Azure Blob Storage.")
 
-    consolidate_runs(connection_string, container_name, k=10)
+    workflow_run_group_by_testcase = consolidate_runs(connection_string, silver_container_name, k=10)
+    
+    upload_blob(
+        connection_string,
+        gold_container_name,
+        "gold-report.json",
+        json.dumps(workflow_run_group_by_testcase),
+    )
